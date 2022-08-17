bfa052dc3323a5c1aca2519fb7588c3624cdc3bb
Burulai <burulaiurbaeva13@gmail.com>
2022-08-17 18:05:16 +0600

    tapshyrma16

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
@@ -8,16 +10,26 @@ public class Main {
         Turtle turtle = new Turtle();
 
         Animal[] animals = {animal, shark, eagle, turtle};
+        Shark[] sharks = new Shark[1];
+        Eagle[] eagles = new Eagle[1];
+        Turtle[] turtles = new Turtle[1];
 
         for (Animal i: animals) {
-            if (i.getClass().getSimpleName().equals("Animal"))
+            if (i.getClass().getSimpleName().equals("Animal")) {
                 i.animalMethod();
-            else if (i instanceof Eagle)
-                ((Eagle)i).fly();
-            else if (i instanceof Shark)
-                ((Shark)i).attack();
-            else if (i instanceof Turtle)
-                ((Turtle)i).swim();
+            }
+            else if (i instanceof Eagle) {
+                ((Eagle) i).fly();
+                eagles[0] = (Eagle) i;
+            }
+            else if (i instanceof Shark) {
+                ((Shark) i).attack();
+                sharks[0] = (Shark) i;
+            }
+            else if (i instanceof Turtle) {
+                ((Turtle) i).swim();
+                turtles[0] = (Turtle) i;
+            }
         }
     }
 }
